04d7925023b71f69185b50064e0927f3776d1ef6
TnTeddy96 <wjddns363@naver.com>
2022-03-27 02:02:24 +0900

    15596 정수 n개의 합

---
+++ algorithm_study/src/algorithm_study/Sum_of_N.java
@@ -0,0 +1,14 @@
+package algorithm_study;
+
+public class Sum_of_N {
+	long sum(int[] a_) {
+		long ans = 0;
+		for(int i=0;i<a.length;i++)
+			ans +=a[i];
+		return ans;
+	}
+
+	public static void main(String[] args) {
+	}
+
+}
