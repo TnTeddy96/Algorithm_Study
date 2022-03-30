0b9d480ea3acd96185d3a2200394a8f4794e7b25
TnTeddy96 <wjddns363@naver.com>
2022-03-30 19:05:56 +0900

    1152 단어의 갯수

--- algorithm_study/src/Main.java
+++ algorithm_study/src/Main.java
@@ -1,47 +1,15 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main{
-	public static int count(String g, int m) {
-		char alp = 'A';
-		alp = ((char)((int)alp + m));
-		int count = 0;
-		for(int i= 0; i<g.length();i++) {
-			if((g.charAt(i)==alp)||(g.charAt(i)==((char)((int)alp+32)))) {
-				count ++;
-			}
-		}
-		return count;
-	}
-	public static void compare(int arr[]) {
-		int count=0;
-		int where =0;
-		int i_where = 0;
-		char alp = 'A';
-		for(int  i = 0; i<arr.length;i++) {
-			if(arr[i]==0)
-				continue;
-			else
-				if(where<arr[i]) {
-					where = arr[i];
-					count = 0;
-					i_where=i;
-				}
-				else if(where==arr[i]) {
-					count++;
-				}
-		}
-		alp =(char)((int)alp + i_where);
-		if(count>0) 
-			alp = '?';
-		System.out.println(alp);
-	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String given = sc.next();
-		int[] countArray = new int[26]; //return 받은 카운트를 저장할 배열
-		
-		for(int i = 0; i<26; i++) 
-			countArray[i] = count(given, i);
-		compare(countArray);
+		int count= 0;
+		String s = sc.nextLine();
+		StringTokenizer st  = new StringTokenizer(s, " ");
+		while(st.hasMoreTokens()) {
+			st.nextToken();
+			count++;
+		}
+		System.out.print(count);
 	}
 }
