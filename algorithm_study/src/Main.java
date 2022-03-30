2a66fe1884a5bcfaed629fd9548b151d9f8667f5
TnTeddy96 <wjddns363@naver.com>
2022-03-30 14:28:40 +0900

    2675 문자열반복

--- algorithm_study/src/Main.java
+++ algorithm_study/src/Main.java
@@ -1,25 +1,30 @@
+import java.util.ArrayList;
 import java.util.Scanner;
-public class Main {
 
-	public static void find(String w) {
-		char where = 'a';
-		int i_where=0;
-		for(int i =0;i<26;i++) {
-			for(int j = 0; j<w.length();j++) {
-				if(w.charAt(j) ==where){
-					i_where=j;
-					break;
-				}
-				else
-					i_where=-1;
+public class Main{
+	public static String  make(String s, int r) {
+		char g;
+		String P="";
+		for(int i=0;i<s.length();i++) {	
+			for(int j =0;j<r;j++) {
+				g=s.charAt(i);
+				P = P + g;
 			}
-			where++;
-			System.out.print(i_where + " ");
+		}
+		return P;
+	}
+	public static void  main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		ArrayList<String>  list = new ArrayList<>();
+		int many = sc.nextInt();
+		System.out.println();
+		for(int i =0;i<many;i++) {
+			int r = sc.nextInt();
+			String s =sc.next();
+			list.add(make(s, r));
+		}
+		for(int i =0;i<many;i++) {
+			System.out.println(list.get(i));
 		}
 	}
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String s = sc.next();
-		find(s);
-	}
-}
+}		
