ca2824c9031cd784c919e3769d493c89cb49b31f
TnTeddy96 <wjddns363@naver.com>
2022-03-30 18:26:47 +0900

    1157 단어공부

--- algorithm_study/src/Main.java
+++ algorithm_study/src/Main.java
@@ -1,30 +1,47 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main{
-	public static String  make(String s, int r) {
-		char g;
-		String P="";
-		for(int i=0;i<s.length();i++) {	
-			for(int j =0;j<r;j++) {
-				g=s.charAt(i);
-				P = P + g;
+	public static int count(String g, int m) {
+		char alp = 'A';
+		alp = ((char)((int)alp + m));
+		int count = 0;
+		for(int i= 0; i<g.length();i++) {
+			if((g.charAt(i)==alp)||(g.charAt(i)==((char)((int)alp+32)))) {
+				count ++;
 			}
 		}
-		return P;
+		return count;
 	}
-	public static void  main(String[] args) {
+	public static void compare(int arr[]) {
+		int count=0;
+		int where =0;
+		int i_where = 0;
+		char alp = 'A';
+		for(int  i = 0; i<arr.length;i++) {
+			if(arr[i]==0)
+				continue;
+			else
+				if(where<arr[i]) {
+					where = arr[i];
+					count = 0;
+					i_where=i;
+				}
+				else if(where==arr[i]) {
+					count++;
+				}
+		}
+		alp =(char)((int)alp + i_where);
+		if(count>0) 
+			alp = '?';
+		System.out.println(alp);
+	}
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		ArrayList<String>  list = new ArrayList<>();
-		int many = sc.nextInt();
-		System.out.println();
-		for(int i =0;i<many;i++) {
-			int r = sc.nextInt();
-			String s =sc.next();
-			list.add(make(s, r));
-		}
-		for(int i =0;i<many;i++) {
-			System.out.println(list.get(i));
-		}
+		String given = sc.next();
+		int[] countArray = new int[26]; //return 받은 카운트를 저장할 배열
+		
+		for(int i = 0; i<26; i++) 
+			countArray[i] = count(given, i);
+		compare(countArray);
 	}
-}		
+}
