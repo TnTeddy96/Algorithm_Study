e8b418f3f6b25bffb1ae758a5687645d36916d55
TnTeddy96 <wjddns363@naver.com>
2022-03-31 22:18:36 +0900

    5622 다이얼

--- algorithm_study/src/Main.java
+++ algorithm_study/src/Main.java
@@ -1,21 +1,42 @@
 import java.util.Scanner;
 
 public class Main{
-	public static int SangSoo(int i) {
-		i+= (i%10)*100-(i%10)+(i/100)-(i/100)*100;
-		return i;
-	}
-	public static void compareNum(int i, int j) {
-		if(i>j)
-			System.out.println(i);
-		else
-			System.out.println(j);
+	public static int time(String d, int i) {
+		int vlaue=0;
+		switch(d.charAt(i)) {
+		case 'A':	case 'B':	case 'C':
+			vlaue=3;
+			break;
+		case 'D':	case 'E':	case 'F':
+			vlaue=4;
+			break;
+		case 'G':	case 'H':	case 'I':
+			vlaue=5;
+			break;
+		case 'J':case 'K':case 'L':
+			vlaue=6;
+			break;
+		case 'M':case 'N':case 'O':
+			vlaue=7;
+			break;
+		case 'P':case 'Q':case 'R':case 'S':
+			vlaue=8;
+			break;
+		case 'T':case 'U':case 'V':
+			vlaue=9;
+			break;
+		case 'W':case 'X':case 'Y':case 'Z':
+			vlaue=10;
+			break;
+		}
+			return vlaue;
 	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int num1 = sc.nextInt();
-		int num2 = sc.nextInt();
-		
-		compareNum(SangSoo(num1),SangSoo(num2));
+		String dial = sc.next();
+		int sum =0;
+		for(int i =0;i<dial.length();i++)
+			sum+=time(dial, i);
+		System.out.println(sum);
 	}
 }
