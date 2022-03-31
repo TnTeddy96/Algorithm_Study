1a4d6ea26e7b408c9cc824aa7710417ab07a36f5
TnTeddy96 <wjddns363@naver.com>
2022-03-31 21:39:26 +0900

    2908 상수

--- algorithm_study/src/Main.java
+++ algorithm_study/src/Main.java
@@ -1,15 +1,21 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main{
+	public static int SangSoo(int i) {
+		i+= (i%10)*100-(i%10)+(i/100)-(i/100)*100;
+		return i;
+	}
+	public static void compareNum(int i, int j) {
+		if(i>j)
+			System.out.println(i);
+		else
+			System.out.println(j);
+	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int count= 0;
-		String s = sc.nextLine();
-		StringTokenizer st  = new StringTokenizer(s, " ");
-		while(st.hasMoreTokens()) {
-			st.nextToken();
-			count++;
-		}
-		System.out.print(count);
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		
+		compareNum(SangSoo(num1),SangSoo(num2));
 	}
 }
