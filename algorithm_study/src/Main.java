c2ba2e3c8b8c8a0beac5728e2507320d93f2203c
TnTeddy96 <wjddns363@naver.com>
2022-04-06 02:17:05 +0900

    1712 손익분기점

--- algorithm_study/src/Main.java
+++ algorithm_study/src/Main.java
@@ -1,36 +1,20 @@
 import java.util.Scanner;
 
 public class Main{
-	static boolean groupWordsChecker(String g){
-
-		for(int i= 0;i<g.length();i++) {
-			int where = 0;
-			for(int k = 1; k<g.length();k++) {
-				if((Character.compare(g.charAt(k-1),g.charAt(i)) == 0 )&&(Character.compare(g.charAt(i), g.charAt(k))!=0)) {
-					where=k+1;
-					break;
-				}
-			}
-			if(where==0)
-				return true;
-			for(int p = where ; p<g.length();p++) {
-				if(g.charAt(p)==g.charAt(i)) {
-					return false;
-				}
-			}
+	static int breakEvenPoint(int f, int v, int p) {
+		if(p-v<=0)
+			return -1;
+		else {
+			return (((p-v)+f)/(p-v));
 		}
-		return true;
+			
 	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		int many = sc.nextInt();
-		int count = 0;
-		for(int i = 0; i<many;i++) {
-			String groupWords = sc.next();
-			if(groupWordsChecker(groupWords)==true)
-				count+=1;
-		}
-		System.out.print(count);
+		int fixMoney = sc.nextInt();
+		int varMoney = sc.nextInt();
+		int price = sc.nextInt();
+		System.out.println(breakEvenPoint(fixMoney, varMoney, price));
+	
 	}
 }
