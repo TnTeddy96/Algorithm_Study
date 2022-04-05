9209fe61c837bffe2e2a334feb000b8e227c52fc
TnTeddy96 <wjddns363@naver.com>
2022-04-05 22:33:45 +0900

    2941 크로아티아 알파벳

--- algorithm_study/src/Main.java
+++ algorithm_study/src/Main.java
@@ -1,42 +1,29 @@
 import java.util.Scanner;
 
 public class Main{
-	public static int time(String d, int i) {
-		int vlaue=0;
-		switch(d.charAt(i)) {
-		case 'A':	case 'B':	case 'C':
-			vlaue=3;
-			break;
-		case 'D':	case 'E':	case 'F':
-			vlaue=4;
-			break;
-		case 'G':	case 'H':	case 'I':
-			vlaue=5;
-			break;
-		case 'J':case 'K':case 'L':
-			vlaue=6;
-			break;
-		case 'M':case 'N':case 'O':
-			vlaue=7;
-			break;
-		case 'P':case 'Q':case 'R':case 'S':
-			vlaue=8;
-			break;
-		case 'T':case 'U':case 'V':
-			vlaue=9;
-			break;
-		case 'W':case 'X':case 'Y':case 'Z':
-			vlaue=10;
-			break;
+	static int manyAlpha(String c) {
+		int count = 1;
+		for( int i = 1; i<c.length();i++) {
+			if(c.charAt(i)=='-')
+				continue;
+			else if(c.charAt(i)=='j') { 
+				if(c.charAt(i-1)=='l' || c.charAt(i-1)=='n')
+					continue;
+			}
+			else if(c.charAt(i)=='=') {
+				if(i>=2) {
+					if(c.charAt(i-1)=='z'&&c.charAt(i-2)=='d') 
+						count--;
+				}
+				continue;
+			}
+			count++;
 		}
-			return vlaue;
+		return count;
 	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String dial = sc.next();
-		int sum =0;
-		for(int i =0;i<dial.length();i++)
-			sum+=time(dial, i);
-		System.out.println(sum);
+		String cAlpha = sc.next();
+		System.out.println(manyAlpha(cAlpha));
 	}
 }
