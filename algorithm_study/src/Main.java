5dcef9bfe0ab8bc13697c8c184e70545ba3822ec
TnTeddy96 <wjddns363@naver.com>
2022-04-06 01:29:13 +0900

    1316 그룹단어 체커

--- algorithm_study/src/Main.java
+++ algorithm_study/src/Main.java
@@ -1,29 +1,36 @@
 import java.util.Scanner;
 
 public class Main{
-	static int manyAlpha(String c) {
-		int count = 1;
-		for( int i = 1; i<c.length();i++) {
-			if(c.charAt(i)=='-')
-				continue;
-			else if(c.charAt(i)=='j') { 
-				if(c.charAt(i-1)=='l' || c.charAt(i-1)=='n')
-					continue;
-			}
-			else if(c.charAt(i)=='=') {
-				if(i>=2) {
-					if(c.charAt(i-1)=='z'&&c.charAt(i-2)=='d') 
-						count--;
+	static boolean groupWordsChecker(String g){
+
+		for(int i= 0;i<g.length();i++) {
+			int where = 0;
+			for(int k = 1; k<g.length();k++) {
+				if((Character.compare(g.charAt(k-1),g.charAt(i)) == 0 )&&(Character.compare(g.charAt(i), g.charAt(k))!=0)) {
+					where=k+1;
+					break;
 				}
-				continue;
 			}
-			count++;
+			if(where==0)
+				return true;
+			for(int p = where ; p<g.length();p++) {
+				if(g.charAt(p)==g.charAt(i)) {
+					return false;
+				}
+			}
 		}
-		return count;
+		return true;
 	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String cAlpha = sc.next();
-		System.out.println(manyAlpha(cAlpha));
+		
+		int many = sc.nextInt();
+		int count = 0;
+		for(int i = 0; i<many;i++) {
+			String groupWords = sc.next();
+			if(groupWordsChecker(groupWords)==true)
+				count+=1;
+		}
+		System.out.print(count);
 	}
 }
