45ba66561c625e0f2d219aad08bde163e1af0ac8
TnTeddy96 <wjddns363@naver.com>
2022-04-06 22:54:55 +0900

    2292 벌집

--- algorithm_study/src/Main.java
+++ algorithm_study/src/Main.java
@@ -1,20 +1,23 @@
 import java.util.Scanner;
 
 public class Main{
-	static int breakEvenPoint(int f, int v, int p) {
-		if(p-v<=0)
-			return -1;
-		else {
-			return (((p-v)+f)/(p-v));
+	
+	static int room(int n) {
+		if(n==1)
+			return n;
+		int min;
+		int max;
+		for(int i = 0;;i++) {
+			min = (int)(2+3*i*(i+1));
+			max =(int)(1+3*(i+1)*(i+2));
+			if((min<=n)&&(n<=max))
+				return i+2;
 		}
-			
 	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int fixMoney = sc.nextInt();
-		int varMoney = sc.nextInt();
-		int price = sc.nextInt();
-		System.out.println(breakEvenPoint(fixMoney, varMoney, price));
-	
+		int num = sc.nextInt();
+		
+		System.out.print(room(num));
 	}
 }
