2b8de6ffa501e33bf642205bad74066f432c682e
TnTeddy96 <wjddns363@naver.com>
2022-04-14 01:34:22 +0900

    7568 덩치 //1018체스판 이 배열인덱스에러로 인해 불가피하게 sMain 으로 클래스 작성

--- algorithm_study/src/Main.java
+++ algorithm_study/src/Main.java
@@ -1,47 +1,80 @@
 import java.util.Scanner;
 
 public class Main{
-	static void save(Scanner sc, int n, int[] nArray){
-		for(int i = 0; i<n;i++) {
-			nArray[i] = sc.nextInt();
+	
+	static int manyLong(String a[][], int n, int m) {
+		int count = 0;	//불일치도
+		String match;
+		String nMatch;
+		if(a[n][m].charAt(0)=='W') {
+			match = "WB";
+			nMatch = "BW";
 		}
-	}
-	static int[] cmp(int[] n, int num, int m) {
-		int[] nArray2=new int[num]; // 큰값을 넣을 배열
-		int sum=0;
-		for(int i = 0;i<n.length;i++) {
-			for(int j = i+1;j<n.length;j++) {
-				if(j==n.length)
-					break;
-				for(int k = j+1;k<n.length;k++) {
-					if(k==n.length)
-						break;
-					else if((n[i]+n[j]+n[k]<=m)&&(n[i]+n[j]+n[k]>sum))
-						sum = n[i] + n[j] + n[k];
+		else {
+			match = "BW";
+			nMatch = "WB";
+		}
+		if(n%2==0) {
+			for(int i = n;i<n+8;i++) {
+				for(int j = m;j<m+8;j=j+2) {
+					if(i%2==1) 
+						if(!(a[i][j] + a[i][j+1]).equals(nMatch)) 
+							count++;
+					else 
+						if(!(a[i][j] + a[i][j+1]).equals(match)) 
+							count++;
 				}
 			}
-			nArray2[i] = sum;
 		}
-		return nArray2;
-	}
-	static int ans(int[] nArray2) {
-		int num=0;
-		
-		for(int i = 0; i<nArray2.length;i++) {
-			if(nArray2[i] >=num)
-				num=nArray2[i];
+		else {
+			for(int i = n;i<n+8;i++) {
+				for(int j = m;j<m+8;j=j+2) {
+					if(i%2==1) 
+						if(!(a[i][j] + a[i][j+1]).equals(nMatch)) 
+							count++;
+					else 
+						if(!(a[i][j] + a[i][j+1]).equals(match)) 
+							count++;
+				}
+			}
 		}
-		return num;
+		return count;
 	}
-	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		sc.nextLine();
+		int num =64;
+		//n*m 보드판
+		String [][] chessB= new String[n][m];
 		
-		int n = sc.nextInt(); // 입력받을 수의 갯수
-		int m = sc.nextInt();// 최대값
-		int[] nArray = new int[n]; //입력받을 수를 저장할 배열 
-		save(sc, n, nArray); // 입력받은 수를 배열에 저장
-		System.out.println(ans(cmp(nArray, n,m)));
+		// 입력된 BW를 배열에 저장
+		for(int i =0;i<n;i++) 
+			for(int j = 0;j<m;j++) 
+				chessB[i][j] = Character.toString(sc.nextLine().charAt(j));
 		
+		for(int i = 0;i<n;i++)
+			for(int j = 0;j<m;j++)
+				System.out.print(chessB[i][j]);
+		//count 를 저장할 Array 선언
+		int[] notMatchNum = new int[(n-7)*(m-7)];
+		
+		for(int i =0;i<n;i++) {
+			if(n-i<7)
+				break;
+			else
+				for(int j=0;j<m;j++) {
+					if(m-j<7)
+						break;
+					else 
+						notMatchNum[j] = manyLong(chessB, i, j);
+				}
+		}
+		for(int i=0;i<notMatchNum.length;i++) {
+			if(notMatchNum[i]<num)
+				num=notMatchNum[i];
+		}
+		System.out.println(num);
 	}
 }
