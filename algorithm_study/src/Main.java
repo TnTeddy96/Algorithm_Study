cdc300d5550ab1aa1211d900fc8e2b122d2b6e3d
TnTeddy96 <wjddns363@naver.com>
2022-04-14 17:53:15 +0900

    1018 체스판다시칠하기

--- algorithm_study/src/Main.java
+++ algorithm_study/src/Main.java
@@ -1,79 +1,79 @@
 import java.util.Scanner;
-
+/*WBWB
+ * BWBW
+ */
 public class Main{
-	
-	static int manyLong(String a[][], int n, int m) {
-		int count = 0;	//불일치도
-		String match;
-		String nMatch;
-		if(a[n][m].charAt(0)=='W') {
-			match = "WB";
-			nMatch = "BW";
-		}
-		else {
-			match = "BW";
-			nMatch = "WB";
-		}
-		if(n%2==0) {
-			for(int i = n;i<n+8;i++) {
-				for(int j = m;j<m+8;j=j+2) {
-					if(i%2==1) 
-						if(!(a[i][j] + a[i][j+1]).equals(nMatch)) 
-							count++;
-					else 
-						if(!(a[i][j] + a[i][j+1]).equals(match)) 
-							count++;
+	static int check1(String a[][], int n, int m) {
+		int count=0;
+		for(int i = n;i<n+8;i++) {
+			for(int j = m;j<m+8;j=j+2) {
+				if(i%2==0) {
+					if(!(a[i][j]+a[i][j+1]).equals("WB") &&(a[i][j]+a[i][j+1]).equals("BW"))
+						count+=2;
+					else if(!(a[i][j]+a[i][j+1]).equals("WB"))
+						count++;
 				}
-			}
-		}
-		else {
-			for(int i = n;i<n+8;i++) {
-				for(int j = m;j<m+8;j=j+2) {
-					if(i%2==1) 
-						if(!(a[i][j] + a[i][j+1]).equals(nMatch)) 
-							count++;
-					else 
-						if(!(a[i][j] + a[i][j+1]).equals(match)) 
-							count++;
+				else {
+					if(!(a[i][j]+a[i][j+1]).equals("BW")&&(a[i][j]+a[i][j+1]).equals("WB"))
+						count+=2;
+					else if(!(a[i][j]+a[i][j+1]).equals("BW"))
+						count++;
 				}
 			}
 		}
 		return count;
 	}
+	/*BWBW
+	 	WBWB
+	 */
+	static int check2(String a[][], int n , int m) {
+		int count=0;
+		for(int i = n;i<n+8;i++) {
+			for(int j = m;j<m+8;j=j+2) {
+				if(i%2==0) {
+					if(!(a[i][j]+a[i][j+1]).equals("BW")&&(a[i][j]+a[i][j+1]).equals("WB"))
+						count+=2;
+					else if(!(a[i][j]+a[i][j+1]).equals("BW"))
+						count++;
+				}
+				else {
+					if(!(a[i][j]+a[i][j+1]).equals("WB")&&(a[i][j]+a[i][j+1]).equals("BW"))
+						count+=2;
+					else if(!(a[i][j]+a[i][j+1]).equals("WB"))
+						count++;
+				}
+			}
+		}
+		return count;
+	}
+	static int manyLong(String a[][], int n, int m) {
+		if(check1(a, n, m)<check2(a, n , m))
+			return check1(a, n , m);
+		else
+			return check2(a, n, m);
+	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int num=32;
 		int n = sc.nextInt();
 		int m = sc.nextInt();
 		sc.nextLine();
-		int num =64;
 		//n*m 보드판
 		String [][] chessB= new String[n][m];
 		
-		// 입력된 BW를 배열에 저장
-		for(int i =0;i<n;i++) 
-			for(int j = 0;j<m;j++) 
-				chessB[i][j] = Character.toString(sc.nextLine().charAt(j));
-		
-		for(int i = 0;i<n;i++)
-			for(int j = 0;j<m;j++)
-				System.out.print(chessB[i][j]);
-		//count 를 저장할 Array 선언
-		int[] notMatchNum = new int[(n-7)*(m-7)];
-		
+		// 입력된 BW를 보드판에 저장
 		for(int i =0;i<n;i++) {
-			if(n-i<7)
-				break;
-			else
-				for(int j=0;j<m;j++) {
-					if(m-j<7)
-						break;
-					else 
-						notMatchNum[j] = manyLong(chessB, i, j);
-				}
+			String line = sc.nextLine();
+			for(int j = 0;j<m;j++) 
+				chessB[i][j] = Character.toString(line.charAt(j));
 		}
-		for(int i=0;i<notMatchNum.length;i++) {
-			if(notMatchNum[i]<num)
-				num=notMatchNum[i];
+
+		//BW를 8*8 만큼만 검사 하고 불일치도(수정해야하는 판의 갯수) 중 작은값 num에 저장
+		for(int i =0;i<n-7;i++) {				//		세로 
+			for(int j = 0;j<m-7;j++) {		//		가로
+				if(manyLong(chessB, i, j)<=num)
+					num=manyLong(chessB, i, j);
+			}
 		}
 		System.out.println(num);
 	}
