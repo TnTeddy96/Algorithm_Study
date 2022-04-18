861ba92aa0de8e5d67e55ee186570877af95cd0b
TnTeddy96 <wjddns363@naver.com>
2022-04-19 01:05:38 +0900

    1436 영화감독 숌

--- algorithm_study/src/Main.java
+++ algorithm_study/src/Main.java
@@ -1,80 +1,39 @@
-import java.util.Scanner;
-/*WBWB
- * BWBW
- */
-public class Main{
-	static int check1(String a[][], int n, int m) {
-		int count=0;
-		for(int i = n;i<n+8;i++) {
-			for(int j = m;j<m+8;j=j+2) {
-				if(i%2==0) {
-					if(!(a[i][j]+a[i][j+1]).equals("WB") &&(a[i][j]+a[i][j+1]).equals("BW"))
-						count+=2;
-					else if(!(a[i][j]+a[i][j+1]).equals("WB"))
-						count++;
+	import java.util.Scanner;
+	import java.util.ArrayList;
+	
+	
+	public class Main{
+		public static String whatsMvn(int m) {
+			ArrayList<Character> list = new ArrayList<Character>();
+			int count = 0;
+			String mvN;
+			for(int i = 666;;i++) {
+				//mvN 을 문자열로 바꾸기
+				 mvN= Integer.toString(i); 
+				 //mvN을 캐릭터 어레이리스트 에 저장
+				for(int j = 0;j<mvN.length();j++) {
+					list.add(j, mvN.charAt(j));
 				}
-				else {
-					if(!(a[i][j]+a[i][j+1]).equals("BW")&&(a[i][j]+a[i][j+1]).equals("WB"))
-						count+=2;
-					else if(!(a[i][j]+a[i][j+1]).equals("BW"))
-						count++;
+				
+				for(int k = 0;k<list.size();k++){
+					if(list.get(k)=='6'&&(k+2)<list.size()) {
+						if(list.get(k+1)=='6'&&list.get(k+1)==list.get(k+2)) {
+							count++;
+							break;
+						}
+					}
 				}
+				//어레이 리스트 초기화
+				list.clear();
+				//m번째 영화라면 반복문 탈출
+				if(count==m)
+					break;
 			}
+			return mvN;
 		}
-		return count;
-	}
-	/*BWBW
-	 	WBWB
-	 */
-	static int check2(String a[][], int n , int m) {
-		int count=0;
-		for(int i = n;i<n+8;i++) {
-			for(int j = m;j<m+8;j=j+2) {
-				if(i%2==0) {
-					if(!(a[i][j]+a[i][j+1]).equals("BW")&&(a[i][j]+a[i][j+1]).equals("WB"))
-						count+=2;
-					else if(!(a[i][j]+a[i][j+1]).equals("BW"))
-						count++;
-				}
-				else {
-					if(!(a[i][j]+a[i][j+1]).equals("WB")&&(a[i][j]+a[i][j+1]).equals("BW"))
-						count+=2;
-					else if(!(a[i][j]+a[i][j+1]).equals("WB"))
-						count++;
-				}
-			}
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			int mvS = sc.nextInt();
+			System.out.println(whatsMvn(mvS));
 		}
-		return count;
 	}
-	static int manyLong(String a[][], int n, int m) {
-		if(check1(a, n, m)<check2(a, n , m))
-			return check1(a, n , m);
-		else
-			return check2(a, n, m);
-	}
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int num=32;
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-		sc.nextLine();
-		//n*m 보드판
-		String [][] chessB= new String[n][m];
-		
-		// 입력된 BW를 보드판에 저장
-		for(int i =0;i<n;i++) {
-			String line = sc.nextLine();
-			for(int j = 0;j<m;j++) 
-				chessB[i][j] = Character.toString(line.charAt(j));
-		}
-
-		//BW를 8*8 만큼만 검사 하고 불일치도(수정해야하는 판의 갯수) 중 작은값 num에 저장
-		for(int i =0;i<n-7;i++) {				//		세로 
-			for(int j = 0;j<m-7;j++) {		//		가로
-				if(manyLong(chessB, i, j)<=num)
-					num=manyLong(chessB, i, j);
-			}
-		}
-		System.out.println(num);
-	}
-}
