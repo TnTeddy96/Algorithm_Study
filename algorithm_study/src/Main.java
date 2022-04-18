c9d8482c55be0ebb185d795f1df16ed63c1c6fff
TnTeddy96 <wjddns363@naver.com>
2022-04-19 02:09:55 +0900

    2750 수 정렬하기

--- algorithm_study/src/Main.java
+++ algorithm_study/src/Main.java
@@ -1,39 +1,29 @@
-	import java.util.Scanner;
-	import java.util.ArrayList;
+import java.util.Scanner;
 	
-	
-	public class Main{
-		public static String whatsMvn(int m) {
-			ArrayList<Character> list = new ArrayList<Character>();
-			int count = 0;
-			String mvN;
-			for(int i = 666;;i++) {
-				//mvN 을 문자열로 바꾸기
-				 mvN= Integer.toString(i); 
-				 //mvN을 캐릭터 어레이리스트 에 저장
-				for(int j = 0;j<mvN.length();j++) {
-					list.add(j, mvN.charAt(j));
+public class Main{
+	static void sort(int[] a) {
+		int num = 0;
+		for(int i = 0; i<a.length; i++) {
+			for(int j = 1; j<a.length;j++) {
+				if(a[j-1]>a[j]) {
+					num = a[j-1];
+					a[j-1] = a[j];
+					a[j] = num;
 				}
-				
-				for(int k = 0;k<list.size();k++){
-					if(list.get(k)=='6'&&(k+2)<list.size()) {
-						if(list.get(k+1)=='6'&&list.get(k+1)==list.get(k+2)) {
-							count++;
-							break;
-						}
-					}
-				}
-				//어레이 리스트 초기화
-				list.clear();
-				//m번째 영화라면 반복문 탈출
-				if(count==m)
-					break;
 			}
-			return mvN;
 		}
-		public static void main(String[] args) {
-			Scanner sc = new Scanner(System.in);
-			int mvS = sc.nextInt();
-			System.out.println(whatsMvn(mvS));
+		for(int k  : a) {
+			System.out.println(k);
 		}
 	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int sizeArr = sc.nextInt();
+		int[] numArr = new int[sizeArr];
+		
+		for(int i =0; i<numArr.length ;i++) {
+			numArr[i] = sc.nextInt();
+		}
+		sort(numArr);
+	}
+}
