4b173d1da52c8631f43c3e12d924db0975e0107e
TnTeddy96 <wjddns363@naver.com>
2022-04-19 02:36:16 +0900

    2751 수정렬하기2 ++ Buffer 이용하기

--- algorithm_study/src/Main.java
+++ algorithm_study/src/Main.java
@@ -1,29 +1,22 @@
+import java.util.Arrays;
 import java.util.Scanner;
+import java.io.*;
 	
 public class Main{
-	static void sort(int[] a) {
-		int num = 0;
-		for(int i = 0; i<a.length; i++) {
-			for(int j = 1; j<a.length;j++) {
-				if(a[j-1]>a[j]) {
-					num = a[j-1];
-					a[j-1] = a[j];
-					a[j] = num;
-				}
-			}
-		}
-		for(int k  : a) {
-			System.out.println(k);
-		}
-	}
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
+		BufferedWriter bf = new BufferedWriter(new OutputStreamWriter(System.out));
 		int sizeArr = sc.nextInt();
 		int[] numArr = new int[sizeArr];
 		
 		for(int i =0; i<numArr.length ;i++) {
 			numArr[i] = sc.nextInt();
 		}
-		sort(numArr);
+		Arrays.sort(numArr);
+		
+		for(int k : numArr)
+			bf.write(k + "\n");
+		bf.close();
 	}
 }
