b3c1ffd3150ac1d613129e95032ff05a18218d68
TnTeddy96 <wjddns363@naver.com>
2022-06-28 16:00:25 +0900

    2562 최댓값

--- algorithm_study/src/Main.java
+++ algorithm_study/src/Main.java
@@ -3,27 +3,26 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+
 
 public class Main{
-	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		String s = br.readLine();
-		StringTokenizer st = new StringTokenizer(s);
-		int A = Integer.parseInt(st.nextToken());
-		int B = Integer.parseInt(st.nextToken());
-		
-		if(A>B)
-			bw.write(">");
-		else if (A==B)
-			bw.write("==");
-		else
-			bw.write("<");
-		
+		int[] arr = new int[9];
+		for(int i = 0; i<arr.length;i++) {
+			arr[i]=Integer.parseInt(br.readLine());
+		}
+		int where= 0;
+		int max = arr[0];
+		for(int i =1;i<arr.length;i++) {
+			if(max<arr[i]) {
+				max=arr[i];
+				where=i + 1;
+			}
+		}
+		bw.write(max + "\n" + where);
 		bw.flush();
-		bw.close();
-		
+		br.close();
 	}
 }
