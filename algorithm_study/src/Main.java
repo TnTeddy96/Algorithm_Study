39360f6e59c84d6bc739a711ffe3dde211b79280
TnTeddy96 <wjddns363@naver.com>
2022-07-01 17:27:04 +0900

    4153 직각 삼각형

--- algorithm_study/src/Main.java
+++ algorithm_study/src/Main.java
@@ -3,33 +3,51 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Main{
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int num =Integer.parseInt(br.readLine());
-		
-		int i;
-		for(i=0;;i++) {
-			if(num<10 || num==12)
-				break;
-			num-=5;
-		}
-		if(num==8) {
-			i=i+1+1;
-		}
-		else if(num==5) {
-			i=i+1;
-		}
-		else if(num==3 || num==6 || num==9 ) {
-			i+=num/3;
-		}
-		else {
-			i=-1;
-		}
-		bw.write(String.valueOf(i));
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args)throws IOException{
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        	ArrayList <Integer> list = new ArrayList<>();
+        while(true){
+            String a = br.readLine();
+            StringTokenizer st = new StringTokenizer(a);
+          
+            int x = Integer.parseInt(st.nextToken());
+            if(x==0)
+            	break;
+            int y = Integer.parseInt(st.nextToken());
+            int z = Integer.parseInt(st.nextToken());
+            if(x>y&&x>z) {
+            	if(x*x==(y*y+z*z)) {
+            		list.add(1);
+            	}
+            	else
+            		list.add(0);
+            }
+            else if(y>z&&y>x) {
+            	if(y*y==(x*x+z*z)) {
+            		list.add(1);
+            	}
+            	else
+            		list.add(0);
+            }
+            else
+            	if(z*z==(x*x + y*y)) {
+            		list.add(1);
+            	}
+            	else
+            		list.add(0);
+        }
+        for(int i =0;i<list.size();i++) {
+        	if(list.get(i)==1)
+        		bw.write("right" + "\n");
+        	else
+        		bw.write("wrong" + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
 }
