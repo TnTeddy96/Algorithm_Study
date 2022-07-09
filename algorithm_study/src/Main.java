eeea25a7bc2bdb7df6a2e7aac7db5bd9f0a3e2d1
TnTeddy96 <wjddns363@naver.com>
2022-07-10 02:17:08 +0900

    2588 곱셈

--- algorithm_study/src/Main.java
+++ algorithm_study/src/Main.java
@@ -1,53 +1,28 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.io.*;
 
 public class Main{
-    public static void main(String[] args)throws IOException{
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        	ArrayList <Integer> list = new ArrayList<>();
-        while(true){
-            String a = br.readLine();
-            StringTokenizer st = new StringTokenizer(a);
-          
-            int x = Integer.parseInt(st.nextToken());
-            if(x==0)
-            	break;
-            int y = Integer.parseInt(st.nextToken());
-            int z = Integer.parseInt(st.nextToken());
-            if(x>y&&x>z) {
-            	if(x*x==(y*y+z*z)) {
-            		list.add(1);
-            	}
-            	else
-            		list.add(0);
-            }
-            else if(y>z&&y>x) {
-            	if(y*y==(x*x+z*z)) {
-            		list.add(1);
-            	}
-            	else
-            		list.add(0);
-            }
-            else
-            	if(z*z==(x*x + y*y)) {
-            		list.add(1);
-            	}
-            	else
-            		list.add(0);
-        }
-        for(int i =0;i<list.size();i++) {
-        	if(list.get(i)==1)
-        		bw.write("right" + "\n");
-        	else
-        		bw.write("wrong" + "\n");
-        }
-        bw.flush();
-        bw.close();
-    }
+	static public void main(String[] args) throws IOException{
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int[] b = new int[3];
+		
+		int firstNum = Integer.parseInt(br.readLine());
+		
+		String secNum=br.readLine();
+		
+		for(int i =0;i<b.length;i++) {
+			b[i]=secNum.charAt(i) - '0';
+		}
+		int j =0;
+		int sum = 0;
+		for(int i =2;i>-1;i--) {
+			bw.write(String.valueOf(firstNum*b[i])+"\n");
+			sum+=firstNum*b[i]*Math.pow(10, j);
+			j++;
+		}
+		bw.write(String.valueOf(sum));
+		bw.flush();
+		bw.close();
+	}
 }
